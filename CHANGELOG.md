b4cbc8ffcefe56534d8230eed282e2a3eb79b404
ukriu <80760888+ukriu@users.noreply.github.com>
2025-01-01 15:08:38 +0530

    download: only get images tagged as cute

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
 # DailyCats
 
+## Release v1.1
+\- Only download images tagged as `cute`
+
 ## Release v1
-Initial Release
+\- Initial Release
--- module/download.sh
+++ module/download.sh
@@ -4,7 +4,7 @@ SAVE_DIR="/storage/emulated/0/Pictures/DailyCat"
 SAVE_PATH="$SAVE_DIR/dailyCat.jpg"
 
 busybox mkdir -p "$SAVE_DIR"
-if busybox wget --no-check-certificate -T 120 -O "$SAVE_PATH" https://cataas.com/cat; then
+if busybox wget --no-check-certificate -T 120 -O "$SAVE_PATH" https://cataas.com/cat/cute; then
     echo "- meow: cat successfully downloaded!"
 else
     echo "- meow: successfully failed to download image!"
